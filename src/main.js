b9b0f6fd181bdccdd80443e9e552d39bf779b5f0
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-02-12 21:51:26 +0500

    Added full web-interface for swaper contract

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import TokenJSON from '../artifacts/contracts/token.sol/Token.json' with { type: 'json' };
 import SwaperJSON from '../artifacts/contracts/swaper.sol/Swaper.json' with { type: 'json' };
-import { BrowserProvider, Contract, formatUnits, parseUnits } from 'ethers';
+import { BrowserProvider, Contract, formatEther, formatUnits, parseEther, parseUnits } from 'ethers';
 
 const tokenAbi = TokenJSON.abi;
 const swaperAbi = SwaperJSON.abi;
@@ -13,6 +13,16 @@ const contractAddresses = {
 };
 
 const elements = {
+    data: {
+      reserve: {
+        tokenA: document.getElementById("tokenAReserve"),
+        tokenB: document.getElementById("tokenBReserve")
+      },
+      balance: {
+        tokenA: document.getElementById("tokenABalance"),
+        tokenB: document.getElementById("tokenBBalance")
+      }
+    },
     reserve: {
         form: document.getElementById("addReserve"),
         inputs: {
@@ -26,6 +36,13 @@ const elements = {
             amount: document.getElementById("swapAmount"),
             isTokenA: document.getElementById("isTokenA")
         }
+    },
+    buy: {
+      form: document.getElementById("buyToken"),
+      inputs: {
+        amount: document.getElementById("tokenAmount"),
+        isTokenA: document.getElementById("isTokenAToBuy")
+      }
     }
 }
 
@@ -38,6 +55,29 @@ let tokenA = null;
 let tokenB = null;
 let swaper = null;
 
+const checkAndApprove = async (tokenContract, amount) => {
+  const currentAllowance = await tokenContract.allowance(userAddress, contractAddresses.swaper);
+  console.log(currentAllowance);
+  if (currentAllowance < amount) {
+      const tx = await tokenContract.approve(
+          contractAddresses.swaper,
+          amount,
+          { gasLimit: 500000 } 
+      );
+      await tx.wait(); 
+      return true;
+  }
+  return false;
+};
+
+async function updateData() {
+   elements.data.balance.tokenA.innerText = formatUnits(await tokenA.balanceOf(userAddress));
+   elements.data.balance.tokenB.innerText = formatUnits(await tokenB.balanceOf(userAddress));
+   elements.data.reserve.tokenA.innerText = formatUnits(await swaper.tokenAReserve());
+   elements.data.reserve.tokenB.innerText = formatUnits(await swaper.tokenBReserve());
+}
+
+
 async function init() {
   try {
     if (!window.ethereum) {
@@ -47,17 +87,14 @@ async function init() {
 
     provider = new BrowserProvider(window.ethereum);
     
-
     const network = await provider.getNetwork();
     console.log('Connected to network:', network.name, network.chainId);
-
 
     const accounts = await provider.send("eth_requestAccounts", []);
     
     if (accounts.length === 0) {
       throw new Error('Аккаунты не обнаружены');
     }
-
 
     signer = await provider.getSigner();
     userAddress = await signer.getAddress();
@@ -67,7 +104,6 @@ async function init() {
       provider,
       signer
     });
-
   } catch (error) {
     console.error('Ошибка инициализации:', error);
     alert(`Ошибка: ${error.message}`);
@@ -108,6 +144,8 @@ async function initializeContracts() {
       signer
     );
 
+    await updateData();
+
     console.log('Контракты инициализированы:', { tokenA, tokenB, swaper });
 
   } catch (error) {
@@ -116,8 +154,8 @@ async function initializeContracts() {
 }
 
 elements.reserve.form.addEventListener('submit', async (event) => {
+  event.preventDefault();
     try {
-        event.preventDefault();
         const tokenAAmountBN = parseUnits(elements.reserve.inputs.first.value.trim(), 18);
         const tokenBAmountBN = parseUnits(elements.reserve.inputs.second.value.trim(), 18);
         
@@ -155,23 +193,73 @@ elements.reserve.form.addEventListener('submit', async (event) => {
         const balanceBAfter = formatUnits(await swaper.tokenBReserve(), 18);
         console.log("Balances after: ", balanceAAfter);
         console.log(balanceBAfter);
+
+        await updateData();
     
     } catch (error) {
         console.error(error.message);        
     }
+});
+
+
+
+elements.swap.form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  try {
+    const swapAmountBN = parseUnits(elements.swap.inputs.amount.value.trim(), 18);
+    const isTokenA = elements.swap.form.isTokenA.checked;
+    const userBalance = isTokenA ? await tokenA.balanceOf(userAddress) : await tokenB.balanceOf(userAddress);
+    if (swapAmountBN > userBalance) {
+      alert("Not enought tokens for swap");
+      return;
+    }
+    if (isTokenA) {
+      await checkAndApprove(tokenA, swapAmountBN);
+    }
+    else {
+      await checkAndApprove(tokenB, swapAmountBN);
+    }
+    const tx = await swaper.swap(swapAmountBN, isTokenA);
+    const receipt = await tx.wait();
+    if (receipt.status === 0) {
+      alert('Swap failed');
+      return;
+    }
+
+    await updateData();
+  } catch (error) {
+    console.error(error.message);
+  }
 })
 
-const checkAndApprove = async (tokenContract, amount) => {
-    const currentAllowance = await tokenContract.allowance(userAddress, contractAddresses.swaper);
-    console.log(currentAllowance);
-    if (currentAllowance < amount) {
-        const tx = await tokenContract.approve(
-            contractAddresses.swaper,
-            amount,
-            { gasLimit: 500000 } 
-        );
-        await tx.wait(); 
-        return true;
+elements.buy.form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  try {
+    const tokenAmount = parseEther(elements.buy.inputs.amount.value.trim(), 18);
+    const isTokenA = elements.buy.inputs.isTokenA.checked;
+    const userBalance = isTokenA ? await tokenA.balanceOf(userAddress) : await tokenB.balanceOf(userAddress);
+
+    if (tokenAmount > userBalance) {
+      alert("Not anought eth for buy");
+      return;
     }
-    return false;
-};
+    
+    const tx = await swaper.buyTokens(isTokenA, {value:tokenAmount, gasLimit:50_000});
+    const receipt = await tx.wait();
+
+    const userBalanceAfter = isTokenA ? await tokenA.balanceOf(userAddress) : await tokenB.balanceOf(userAddress);
+
+    console.log("Balance before: ", userBalance);
+    console.log("Balance after: ", userBalanceAfter);
+
+    if (receipt.status === 0) {
+      alert("Buy failed");
+      return;
+    }
+
+    await updateData();
+
+  } catch (error) {
+    console.error(error.message);
+  }
+});
